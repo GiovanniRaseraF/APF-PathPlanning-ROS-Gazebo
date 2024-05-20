9049bf39c036c18a1eb23833c066bd197dabe54d
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2024-05-20 16:20:19 +0200

    test: just movement

--- test/prototypeV1.py
+++ test/prototypeV1.py
@@ -7,16 +7,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import math
 
 #drone
-drone = np.array([10, 10])
-goal = np.array([40, 40])
+drone = np.array([1, 14])
+goal = np.array([40, 20])
 
 s = 15
-r=2
+r = 0.5
 
 # epsilon
 epsilon = 1
+
+# field
+x = np.arange(-0,50,1)
+y = np.arange(-0,50,1)
+X, Y = np.meshgrid(x,y)
+delx = np.zeros_like(X)
+dely = np.zeros_like(Y)
 
 # plotting
 plt.ion()
@@ -26,17 +34,8 @@ plt.ion()
 # Just for testing and quick prototype dev
 #
 def draw():
-  x = np.arange(-0,50,1)
-  y = np.arange(-0,50,1)
-
   # obstacle
   obstacle = np.array([20, 20])
-
-  # field
-  X, Y = np.meshgrid(x,y)
-
-  delx = np.zeros_like(X)
-  dely = np.zeros_like(Y)
 
   # creazione del potential field
   for i in range(len(x)):
@@ -46,48 +45,48 @@ def draw():
       d_obstacle = np.sqrt((obstacle[0]-X[i][j])**2 + (obstacle[1]-Y[i][j])**2)
 
       #finding theta correspoding to the goal and obstacle 
-      theta_goal= np.arctan2(goal[1] - Y[i][j], goal[0]  - X[i][j])
-      theta_obstacle = np.arctan2(obstacle[1] - Y[i][j], obstacle[0]  - X[i][j])
+      theta_goal = np.arctan2(goal[1] - Y[i][j], goal[0] - X[i][j])
+      theta_obstacle = np.arctan2(obstacle[1] - Y[i][j], obstacle[0] - X[i][j])
 
       if d_obstacle < r:
         delx[i][j] = np.sign(np.cos(theta_obstacle)) +0
-        dely[i][j] = np.sign(np.cos(theta_obstacle))  +0
+        dely[i][j] = np.sign(np.cos(theta_obstacle)) +0
       elif d_obstacle>r+s:
-        delx[i][j] = 0 +(50 * s *np.cos(theta_obstacle))
-        dely[i][j] = 0 + (50 * s *np.sin(theta_goal))
+        delx[i][j] = 0 + (50 * s * np.cos(theta_obstacle))
+        dely[i][j] = 0 + (50 * s * np.sin(theta_goal))
       elif d_obstacle<r+s :
-        delx[i][j] = -120 *(s+r-d_obstacle)* np.cos(theta_obstacle)
-        dely[i][j] = -120 * (s+r-d_obstacle)*  np.sin(theta_obstacle) 
+        delx[i][j] = -120 * (s+r-d_obstacle) * np.cos(theta_obstacle)
+        dely[i][j] = -120 * (s+r-d_obstacle) * np.sin(theta_obstacle) 
       if d_goal <r+s:
         if delx[i][j] != 0:
-          delx[i][j]  += (50 * (d_goal-r) *np.cos(theta_goal))
-          dely[i][j]  += (50 * (d_goal-r) *np.sin(theta_goal))
+          delx[i][j]  += (50 * (d_goal-r) * np.cos(theta_goal))
+          dely[i][j]  += (50 * (d_goal-r) * np.sin(theta_goal))
         else:
-          delx[i][j]  = (50 * (d_goal-r) *np.cos(theta_goal))
-          dely[i][j]  = (50 * (d_goal-r) *np.sin(theta_goal))
+          delx[i][j]  = (50 * (d_goal-r) * np.cos(theta_goal))
+          dely[i][j]  = (50 * (d_goal-r) * np.sin(theta_goal))
         
       if d_goal>r+s:
         if delx[i][j] != 0:
-          delx[i][j] += 50* s *np.cos(theta_goal)
-          dely[i][j] += 50* s *np.sin(theta_goal)
+          delx[i][j] += 50 * s * np.cos(theta_goal)
+          dely[i][j] += 50 * s * np.sin(theta_goal)
         else:
-          delx[i][j] = 50* s *np.cos(theta_goal)
-          dely[i][j] = 50* s *np.sin(theta_goal)
+          delx[i][j] = 50 * s * np.cos(theta_goal)
+          dely[i][j] = 50 * s * np.sin(theta_goal)
 
   fig, ax = plt.subplots(figsize=(10, 10))
   ax.quiver(X, Y, delx, dely)
 
   #obstacle
-  ax.add_patch(plt.Circle(obstacle, r, color='y'))
+  ax.add_patch(plt.Circle(obstacle, r*2, color='y'))
   ax.annotate("Obstacle", xy=obstacle, fontsize=8, ha="center")
 
   #goal
   ax.add_patch(plt.Circle(goal, r, color='m'))
-  ax.annotate("Goal", xy=goal, fontsize=10, ha="center")
+  ax.annotate("Goal", xy=goal, fontsize=1, ha="center")
 
   #drone
   ax.add_patch(plt.Circle(drone, r, color='r'))
-  ax.annotate("Drone", xy=drone, fontsize=7, ha="center")
+  ax.annotate("Drone", xy=drone, fontsize=1, ha="center")
 
   fig.canvas.draw()
   fig.canvas.flush_events()
@@ -101,10 +100,32 @@ def c(d, g):
   ret = np.power(d-g, 2)
   return ret
 
+# distance(x, y) = || x - y ||
+
 # movimento a caso
 while distance(drone, goal) > epsilon:
   # 1. compute the gradient decent of the cost function
+  ck = np.array([0, 0])
+  ck[0] = c(drone, goal)[0] # / dx # dx of c(xk)
+  ck[1] = c(drone, goal)[1] # / dy # dy of c(xk)
+
   # 2. take the oppotite direction
+  Nck = -ck
+  Dk = Nck / distance(ck, np.array([0, 0]))
+
+  print(f"Dk: {Dk}, Nk: {Nck}")
+
+  # 2.1 fake movement
+  droneGridPos = np.array([math.floor(drone[0]), math.floor(drone[1])])
+  forceOnDrone = np.array([
+    delx[droneGridPos[0]][droneGridPos[1]], 
+    dely[droneGridPos[0]][droneGridPos[1]]
+  ])
+  print(f"droneGridPos: {droneGridPos}")
+  print(f"forceOnDrone: {forceOnDrone}")
+
+  drone = drone - Dk
+
   # 3. compute the time step
   # 4. update drone position
 

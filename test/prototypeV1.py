402fc46bc5f0534b4b62a483d1d3ee4572375749
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2024-05-24 16:54:43 +0200

    test: force field fix

--- test/prototypeV1.py
+++ test/prototypeV1.py
@@ -29,7 +29,11 @@ dronePosesX = []
 dronePosesY = []
 
 # intensity modeling
-s = 15  # 
+flag_Goal = True
+flag_Obstacle = True
+α = 50 #
+β = 140
+s = 8
 r = 2
 
 # epsilon
@@ -50,53 +54,47 @@ plt.ion()
 def calcForceField():
   for i in range(len(x)):
     for j in range(len(y)):
+      
       # finding the goal distance and obstacle distance
       d_goal = np.sqrt((goal[0]-X[i][j])**2 + ((goal[1]-Y[i][j]))**2)
       d_obstacle = np.sqrt((obstacle[0]-X[i][j])**2 + (obstacle[1]-Y[i][j])**2)
 
       #finding theta correspoding to the goal and obstacle 
-      theta_goal = np.arctan2(goal[1]-Y[i][j], goal[0]-X[i][j])
-      theta_obstacle = np.arctan2(obstacle[1]-Y[i][j], obstacle[0]-X[i][j])
-
-      if d_obstacle < r:
-        delx[i][j] = np.sign(np.cos(theta_obstacle)) +0
-        dely[i][j] = np.sign(np.cos(theta_obstacle)) +0
-
-      elif d_obstacle>r+s:
-        delx[i][j] = 0 + (50 * s * np.cos(theta_obstacle))
-        dely[i][j] = 0 + (50 * s * np.sin(theta_goal))
-
-      elif d_obstacle<r+s :
-        delx[i][j] = -REP_FORCE * (s+r-d_obstacle) * np.cos(theta_obstacle)
-        dely[i][j] = -REP_FORCE * (s+r-d_obstacle) * np.sin(theta_obstacle) 
-
-      if d_goal <r+s:
-        if delx[i][j] != 0:
-          delx[i][j] += (50 * (d_goal-r) * np.cos(theta_goal))
-          dely[i][j] += (50 * (d_goal-r) * np.sin(theta_goal))
-        else:
-          delx[i][j] = (50 * (d_goal-r) * np.cos(theta_goal))
-          dely[i][j] = (50 * (d_goal-r) * np.sin(theta_goal))
+      theta_goal= np.arctan2(goal[1] - Y[i][j], goal[0]  - X[i][j])
+      theta_obstacle = np.arctan2(obstacle[1] - Y[i][j], obstacle[0]  - X[i][j])
         
-      if d_goal>r+s:
-        if delx[i][j] != 0:
-          delx[i][j] += 50 * s * np.cos(theta_goal)
-          dely[i][j] += 50 * s * np.sin(theta_goal)
-        else:
-          delx[i][j] = 50 * s * np.cos(theta_goal)
-          dely[i][j] = 50 * s * np.sin(theta_goal)
+      if (flag_Obstacle):
+        if d_obstacle < r:
+          delx[i][j] +=0
+          dely[i][j] +=0
+        elif (d_obstacle>=r and d_obstacle<r+s) :
+          delx[i][j] += -β *(s+r-d_obstacle)* np.cos(theta_obstacle)
+          dely[i][j] += -β * (s+r-d_obstacle)* np.sin(theta_obstacle) 
+        elif d_obstacle>=r+s:
+          delx[i][j] +=0 
+          dely[i][j] +=0 
+      
+      if (flag_Goal):    
+        if d_goal <r+s:
+          delx[i][j]  += (50 * (d_goal-r) *np.cos(theta_goal))
+          dely[i][j]  += (50 * (d_goal-r) *np.sin(theta_goal))
+        if d_goal>=r+s:
+          delx[i][j] += 50* s *np.cos(theta_goal)
+          dely[i][j] += 50* s *np.sin(theta_goal)
 
 def draw():
   fig, ax = plt.subplots(figsize=(10, 10))
   ax.quiver(X, Y, delx, dely)
 
   # obstacle
-  ax.add_patch(plt.Circle(obstacle, r, color='y'))
-  ax.annotate("Obstacle", xy=obstacle, fontsize=8, ha="center")
+  if(flag_Obstacle):  
+    ax.add_patch(plt.Circle(obstacle, r, color='y'))
+    ax.annotate("Obstacle", xy=obstacle, fontsize=8, ha="center")
 
   # goal
-  ax.add_patch(plt.Circle(goal, r, color='m'))
-  ax.annotate("Goal", xy=goal, fontsize=1, ha="center")
+  if(flag_Goal):
+    ax.add_patch(plt.Circle(goal, r, color='m'))
+    ax.annotate("Goal", xy=goal, fontsize=8, ha="center")
 
   # drone
   ax.add_patch(plt.Circle(drone, r, color='r'))

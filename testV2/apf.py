e9b1b5ee3c8f2b21e6b1f64f4a34ffee52d234d7
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2025-12-06 21:22:54 +0100

    main: adding field

--- testV2/apf.py
+++ testV2/apf.py
@@ -24,29 +24,54 @@ class Goal2D():
         self.x = np.array(x)
         self.y = np.array(y)
 
+def calc_field(p0: np.array, v_1: np.array, v_2: np.array, c: float = 57.0, t: float = 1) -> np.array:
+    v1_m = np.array([0, 0])
+    v2_m = np.array([0, 0])
+    v1possible = False
+    v2possible = False
+    if (v_1[0] != 0 or v_1[1] != 0):
+        v1_m = v_1 / ((v_1[0] ** 2 + v_1[1] ** 2)**(1/2))
+        v1possible = True
+
+    if (v_2[0] != 0 or v_2[1] != 0):
+        v2_m = v_2 / ((v_2[0] ** 2 + v_2[1] ** 2)**(1/2))
+        v2possible = True
+
+    if(v1possible and v2possible):
+        v1_n = 1 / (np.dot(v_1, v_1) + c) * t
+        v2_n = 1 / (np.dot(v_2, v_2) + c) * t
+        v_3 = p0 + (c + 1) * (v1_m * v1_n + v2_m * v2_n)
+        return v_3
+    else:
+        return p0
+
 class APF2D():
+    
+
     def __init__(self, s_ize: int = DEFAULT_DIM_SIZE):
         print(f"APF2D([{s_ize}][{s_ize}])")
-        self.obstacle: Obstacle2D | None = None
-        self.goal: Goal2D | None = None
         self.s_ize = s_ize
-        self.a = [i + 0.5 for i in range(s_ize)]
-        self.b = [i + 0.5 for i in range(s_ize)]
-        self.p0 = [(x, y) for x in self.a for y in self.b]
-        self.p1 = (-7.9, 0.4)
-        self.p2 = (4.8, 0.56)
-        self.v1 = [(xp1 - self.p1[0], yp1 - self.p1[1]) for (xp1, yp1) in self.p0]
-        self.v2 = [(-(xp2 - self.p2[0]), -(yp2 - self.p2[1])) for (xp2, yp2) in self.p0]
+        self.a = np.array([i for i in range(0, s_ize)])
+        self.b = np.array([i for i in range(0, s_ize)])
+        self.p0 = np.array([(y, x) for x in self.a for y in self.b])
+        self.p1 = np.array([5, 0])
+        self.p2 = np.array([5, 10])
+        self.v1 = np.array([(xp1 - self.p1[0], yp1 - self.p1[1]) for (xp1, yp1) in self.p0])
+        self.v2 = np.array([(-(xp2 - self.p2[0]), -(yp2 - self.p2[1])) for (xp2, yp2) in self.p0])
+        self.c = 100.0
+        self.t = 0.5
+        self.v3 = np.array([calc_field(p, v1, v2, self.c, self.t) for p, v1, v2 in zip(self.p0, self.v1, self.v2)])
+        print(f"v3: {self.v3[:5]}")
 
     # TODO: ragi this needs to update the field ???
     # need to decide if this is a good idea
-    def update_goal(self, new_goal : Goal2D) -> int:
-        self.goal = new_goal
-        return 0
+    # def update_goal(self, new_goal : Goal2D) -> int:
+    #     self.goal = new_goal
+    #     return 0
     
-    def update_obstacle(self, new_obstacle: Obstacle2D) -> int:
-        self.obstacle = new_obstacle
-        return 0
+    # def update_obstacle(self, new_obstacle: Obstacle2D) -> int:
+    #     self.obstacle = new_obstacle
+    #     return 0
 
     def calculate(self) -> None:
         for i in range(self.s_ize):
--- testV2/main.py
+++ testV2/main.py
@@ -7,42 +7,19 @@ from apf import *
 from viz import *
 import time
 
-def main(gridSize=20):
-    goal = Goal2D(gridSize/2, gridSize/2, 1)
-    # obs1 = Obstacle2D(0, 0, 1)
+def main(gridSize=10):
     field = APF2D(gridSize)
 
-    # Add data
-    field.update_goal(goal)
-    # field.insert_obstacle(obs1)
-
-    # Calculate APF
-    field.calculate()
-
-    # Draw
-    vis, data3d = draw(field)
-    vis.create_window(width=1000, height=1000)
-
-    for x in range(gridSize):
-        for y in range(gridSize):
-            # field.obstacles[0].x = x
-            # field.obstacles[0].y = y
-            field.goal.x = x
-            field.goal.y = y
-            field.calculate()
-            vis, data3d = draw(field)
-            vis.create_window(width=1000, height=1000)
-
-            for m in data3d:
-                vis.add_geometry(m)
-            vis.poll_events()
-            vis.update_renderer()
-
-            time.sleep(0.5)
-            vis.destroy_window()
+    # vis, data3d = draw(field)
+    # vis.create_window(width=1000, height=1000)
+    # for m in data3d:
+    #     vis.add_geometry(m)
+    # vis.poll_events()
+    # vis.update_renderer()
     
     # Exit
-    input("Enter to exit...")
+    # input("Enter to exit...")
+    # vis.destroy_window()
 
 if __name__ == "__main__":
-    main()
+    main(10)
